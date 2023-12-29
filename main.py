d9eb18765a627acce9c9b00961bd7914cf245d53
Jhones <jhonesaly@gmail.com>
2023-12-28 22:46:02 -0300

    feat: any pdf input

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from nltk.tokenize import word_tokenize
 from collections import Counter
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 # Baixe os recursos necessários do NLTK
 nltk.download('punkt')
@@ -38,14 +39,16 @@ def load_ignore_words(ignore_file_path):
         print(f'O arquivo de ignore "{ignore_file_path}" não foi encontrado.')
         return []
 
-def generate_csv_and_plot(word_list):
+def generate_csv_and_plot(word_list, csv_path):
     # Gere a análise de frequência
     word_freq = Counter(word_list)
 
     # Crie um DataFrame e salve em um arquivo CSV (todas as palavras)
     df = pd.DataFrame(list(word_freq.items()), columns=['Palavra', 'Frequência'])
     df = df.sort_values(by='Frequência', ascending=False)
-    df.to_csv('output/output.csv', index=False)
+
+    # Salve o DataFrame em um arquivo CSV específico para cada PDF
+    df.to_csv(csv_path, index=False)
 
     # Limite o DataFrame às 20 palavras mais frequentes
     df_top_20 = df.head(20)
@@ -58,17 +61,29 @@ def generate_csv_and_plot(word_list):
     plt.show()
 
 if __name__ == '__main__':
-    # Substitua 'seu_arquivo.pdf' pelo caminho do seu arquivo PDF
-    pdf_path = 'input/origin.pdf'
+    # Caminho para a pasta que contém os arquivos PDF
+    input_folder = 'input/'
 
     # Carregue as palavras a serem ignoradas
     ignore_words = load_ignore_words('ignore.txt')
 
-    # Extraia texto do PDF
-    text = extract_text_from_pdf(pdf_path)
+    # Lista todos os arquivos na pasta input
+    pdf_files = [file for file in os.listdir(input_folder) if file.endswith('.pdf')]
 
-    # Processamento de texto
-    processed_text = process_text(text, ignore_words)
+    # Processa cada arquivo PDF na pasta input
+    for pdf_file in pdf_files:
+        # Caminho completo do arquivo PDF
+        pdf_path = os.path.join(input_folder, pdf_file)
 
-    # Gere CSV e gráfico
-    generate_csv_and_plot(processed_text)
+        # Extraia texto do PDF
+        text = extract_text_from_pdf(pdf_path)
+
+        # Processamento de texto
+        processed_text = process_text(text, ignore_words)
+
+        # Gere um nome de arquivo CSV único com base no nome do arquivo PDF
+        csv_file = os.path.splitext(pdf_file)[0] + '_output.csv'
+        csv_path = os.path.join('output/', csv_file)
+
+        # Gere CSV e gráfico para cada arquivo PDF
+        generate_csv_and_plot(processed_text, csv_path)
