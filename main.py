30e0e66638dd0a0e2f0c3750792ddba1761152bd
Jhones <jhonesaly@gmail.com>
2023-12-28 22:17:46 -0300

    feat: csv completo

--- main.py
+++ main.py
@@ -42,20 +42,16 @@ def generate_csv_and_plot(word_list):
     # Gere a análise de frequência
     word_freq = Counter(word_list)
 
-    # Crie um DataFrame e salve em um arquivo CSV
+    # Crie um DataFrame e salve em um arquivo CSV (todas as palavras)
     df = pd.DataFrame(list(word_freq.items()), columns=['Palavra', 'Frequência'])
-
-    # Classifique o DataFrame em ordem decrescente de frequência
     df = df.sort_values(by='Frequência', ascending=False)
-
-    # Limite o DataFrame às 20 palavras mais frequentes
-    df = df.head(20)
-
-    # Salve o DataFrame em um arquivo CSV
     df.to_csv('output/output.csv', index=False)
 
-    # Crie um gráfico de barras
-    df.plot(kind='bar', x='Palavra', y='Frequência', legend=False)
+    # Limite o DataFrame às 20 palavras mais frequentes
+    df_top_20 = df.head(20)
+
+    # Crie um gráfico de barras (apenas as 20 palavras mais frequentes)
+    df_top_20.plot(kind='bar', x='Palavra', y='Frequência', legend=False)
     plt.title('Análise de Frequência de Palavras (Top 20)')
     plt.xlabel('Palavra')
     plt.ylabel('Frequência')
